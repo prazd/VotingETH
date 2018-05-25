6b40d12ec5062f14223559da0da5079a9cb9f164
prazd <alexokda@list.ru>
2018-05-25 16:24:29 +0300

    voted

--- Smart.sol
+++ Smart.sol
@@ -7,40 +7,44 @@ contract Vote {
     mapping(uint8 => address) voters; //Список голосующих
     mapping(uint8 => bool) sentence; // Голоса
     uint8 sent_counter = 0; // Счетчик голосов
-
     
-    
-    function voting(bool _sent){ //Отдать голос
-       require(sent_counter < 8);
+    function voting(bool _sent) public returns(string) { //Отдать голос
+        require(sent_counter < 8);
+        uint8 cou_acc;
+        for(cou_acc = 0;cou_acc<sent_counter;cou_acc++){ //Проверка, на наличие аккаунта в словаре для проголосовавших
+            if(voters[cou_acc] == msg.sender){
+                return "You have already voted ";
+            }
+        }
         sentence[sent_counter] = _sent;
         voters[sent_counter] = msg.sender;
         sent_counter++;
     } 
     
-    function sent_value() constant returns(uint8){ // Количество голосов
+    function sent_value() constant returns(uint8){             // Количество голосов
         return sent_counter;
     }
     
-    function voters_value(uint8 _i) view returns(address){ // Проголосовавшие
+    function voters_value(uint8 _i) view returns(address){     // Проголосовавшие по 1
         int i;
         for(i = 0; i < sent_counter;i++){
             return voters[_i];
         }
     }
     
-    function null_count() constant { // Завершение голосования
+    function null_count()  {                                   // Завершение голосования
         sent_counter = 0;
     }
     
-    function count_of_vote() view returns(string){ //Подсчет голосов
+    function count_of_vote() view returns(string){             //Подсчет голосов
         uint8 loose;
         uint8 win;
         uint8 i;
         for(i = 0;i<8;i++){
-            if(sentence[i] == true){
+            if(sentence[i] == false){
                 loose++;
             }
-            else if(sentence[i] == false){
+            else if(sentence[i] == true){
                 win++;
             }
             
@@ -52,4 +56,5 @@ contract Vote {
             }
         }
     }
+    
 }
