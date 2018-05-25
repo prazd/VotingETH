33b5ee4cbb861faa9d8a1aaf31bedb59a1313cdd
prazd <alexokda@list.ru>
2018-05-25 19:18:27 +0300

    smart

--- Smart.sol
+++ Smart.sol
@@ -1,17 +1,14 @@
 pragma solidity ^0.4.22;
 //Vote
 //8 - max
-
 contract Vote {
-    
     mapping(uint8 => address) voters; //Список голосующих
     mapping(uint8 => bool) sentence; // Голоса
     uint8 sent_counter = 0; // Счетчик голосов
     
     function voting(bool _sent) public returns(string) { //Отдать голос
         require(sent_counter < 8);
-        uint8 cou_acc;
-        for(cou_acc = 0;cou_acc<sent_counter;cou_acc++){ //Проверка, на наличие аккаунта в словаре для проголосовавших
+        for(uint8 cou_acc = 0;cou_acc<sent_counter;cou_acc++){ //Проверка, на наличие аккаунта в словаре для проголосовавших
             if(voters[cou_acc] == msg.sender){
                 return "You have already voted ";
             }
@@ -26,8 +23,7 @@ contract Vote {
     }
     
     function voters_value(uint8 _i) view returns(address){     // Проголосовавшие по 1
-        int i;
-        for(i = 0; i < sent_counter;i++){
+        for(int i = 0; i < sent_counter;i++){
             return voters[_i];
         }
     }
@@ -39,8 +35,7 @@ contract Vote {
     function count_of_vote() view returns(string){             //Подсчет голосов
         uint8 loose;
         uint8 win;
-        uint8 i;
-        for(i = 0;i<8;i++){
+        for(uint8 i = 0;i<8;i++){
             if(sentence[i] == false){
                 loose++;
             }
