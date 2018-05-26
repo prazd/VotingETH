e673eb80c1f63e76156249640cd688a2101526d4
prazd <alexokda@list.ru>
2018-05-26 13:23:23 +0300

    bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
+#При помощи этого бота можно будет проголосовать
+
 import telebot
 import web3
-//При помощи этого бота можно будет проголосовать 
 bot = telebot.TeleBot('451678818:AAHZvN_Yh5uph4T69JvvDzGQw8mX8h5YA5U')
 @bot.message_handler(commands=["start"])
 def mes(message):
